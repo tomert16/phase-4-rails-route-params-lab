61b362a08cc7d2d1197a90fdc6f2f32e392e21b0
tomer <tomertal52@yahoo.com>
2023-01-30 17:04:48 -0500

    commit tree d43234dbbf91962289b06b86c431139ce74deea2

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   get '/students', to: 'students#index'
   get '/students/grades', to: 'students#grades'
   get '/students/highest-grade', to: 'students#highest_grade'
+
+  get 'students/:id', to: 'students#individual_student'
+
+
 end
